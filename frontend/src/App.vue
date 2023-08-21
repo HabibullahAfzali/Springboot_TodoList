834a31d1ee13c7993cbe2f5ab18ae54e4d7345dc
HabibullahAfzali <hbafzali@gmail.com>
2023-08-21 15:23:35 +0200

    project configuration

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+
+  <div id="app">
+    <h1>HOla</h1>
+  
+  </div>
+  <RouterView />
+</template>
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<script>
+  import WelcomeVue from '../components/WelcomeVue.vue';
+  // import axios from 'axios';
+
+   export default{
+     name: 'welcomeView',
+     component: {
+       WelcomeVue
+     }
+   }
+</script>
+
+<template>
+
+  <main>
+    <welcomeView />
+  </main>
+</template>
