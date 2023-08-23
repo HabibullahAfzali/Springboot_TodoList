30c969855fee775ae7fc403fae6eeb51ff47ec74
kerotupy@hotmail.com <kerotupy@hotmail.com>
2023-08-23 22:51:44 +0200

    avanzamos DevList

--- frontend/src/views/Task.vue
+++ frontend/src/views/Task.vue
@@ -8,30 +8,68 @@ import { useRouter } from 'vue-router';
 
 <template>
     <Navbar />
-    <table class="table">
-  <thead>
-    <tr class="table-primary">
-      <th scope="col"></th>
-      <th scope="col">Tarea</th>
-      <th scope="col">Descripción</th>
+
+    <div class="container my-5 py-5 card bg-glass col-md-6 custom-card">
+        <h1 class="text-center mb-5">DevList</h1>
+            <table class="table custom-table">
+                <thead>
+                    <tr class="table-secondary custom-row">           
+                        <th scope="col" class="col-4">Tarea</th>
+                        <th scope="col" class="">Descripción</th>         
+                    </tr>
+                </thead>
+            </table>
+        <h2>Pendientes</h2>
+            <table class="table custom-table">
+                <tbody>
+                    <tr class="custom-row">
+                        <td class="col-md-4">Mark</td>
+                        <td>Otto</td>       
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table custom-table">
+        <h2>Finalizadas</h2>
+                <tbody>
+                    <tr class="table-danger custom-row"> 
+                        <td class="col-md-4">Jacob</td>
+                        <td>Thornton</td>     
+                    </tr>         
+                </tbody>
+            </table>   
+
       
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      
-    </tr>
-    <tr class="table-danger">
-      <th scope="row"></th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      
-    </tr>
-   
-  </tbody>
-</table>
+    </div>
+  
   
 </template>
+
+<style scope>
+  .custom-table {
+    border-collapse: collapse;
+    width: 100%;
+    
+  }
+
+  .custom-table th,
+  .custom-table td {
+    border: 3px solid #000000;
+    padding: 8px;
+    text-align: center;
+   
+  }
+  
+  .custom-table .custom-row {
+    height: 50px; 
+  }
+
+  .custom-card {
+    background-image: url('../images/imagen_fondo.png');
+    background-size: cover; 
+    background-position: center; 
+  }
+  h1, h2 {
+    color: white;
+  }
+ 
+</style>
