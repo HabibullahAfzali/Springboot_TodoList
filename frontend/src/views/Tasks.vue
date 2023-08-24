76a1016b9620e74a78e31a6fcb665dddf8669fe7
HabibullahAfzali <hbafzali@gmail.com>
2023-08-24 09:19:25 +0200

    style added to Todolis

--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -58,7 +58,7 @@ onMounted(() => {
         <div class="container">
             <div class="row ">
                 <div class="col-md-12">
-                    <h1 class="text-center">Your Todo List</h1>
+                    <h1 class="text-center" style="color: hsl(218, 81%, 75%)">Your Todo List</h1>
                     <!--Add button -->
                     <div class="d-flex justify-content-end" id="newtask">
                         <a href="/addtask" class="btn btn-info">New Task</a>
